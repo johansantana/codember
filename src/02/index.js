70b6f17cf73335af10f8d0cb9aa2dba0bdd2ee32
Johan Santana <johancarlosst@gmail.com>
2022-11-19 10:46:41 -0400

    feat: delete encrypted.txt

--- src/02/index.js
+++ src/02/index.js
@@ -1,19 +1,19 @@
 const TEXT = '11610497110107115 102111114 11210897121105110103 9911110010110998101114 11210810197115101 11510497114101';
 const format = (text) => {
-    const codeArrays = [];
+    const codesArray = [];
     let currentCode = '';
     text.split('').forEach(c => {
         if (c === ' ') {
-            codeArrays.push(c);
+            codesArray.push(c);
             return;
         }
         currentCode += c;
         if (Number(currentCode) >= 97 && Number(currentCode) <= 122) {
-            codeArrays.push(currentCode);
+            codesArray.push(currentCode);
             currentCode = '';
         }
     });
-    return codeArrays;
+    return codesArray;
 };
 const decodeASCII = (text) => {
     const codeArrays = format(text);
