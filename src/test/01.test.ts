66b7ce51ce5424603857a2e5decd21ed332e621e
Johan Santana <johancarlosst@gmail.com>
2022-11-20 17:14:56 -0400

    feat: moved tests to typescript

--- src/test/01.test.ts
+++ src/test/01.test.ts
@@ -5,24 +5,24 @@ import { parseUsers, isValidUser } from '../01/index.js'
 test('Test 01', () => {
   const user = 'usr:@midudev eme:mi@gmail.com psw:123456 age:22 loc:bcn fll:82'
   const parsedUser = parseUsers(user)
-  assert.strictEqual(isValidUser(...parsedUser), true)
+  assert.strictEqual(isValidUser(parsedUser[0]), true)
 })
 
 test('Test 02', () => {
   const user =
     'fll:111 eme:yrfa@gmail.com usr:@codember psw:123456 age:21 loc:World'
   const parsedUser = parseUsers(user)
-  assert.strictEqual(isValidUser(...parsedUser), true)
+  assert.strictEqual(isValidUser(parsedUser[0]), true)
 })
 
 test('Test 03', () => {
   const user = 'psw:11133 loc:Canary fll:333 usr:@pheralb eme:pheralb@gmail.com'
   const parsedUser = parseUsers(user)
-  assert.strictEqual(isValidUser(...parsedUser), false)
+  assert.strictEqual(isValidUser(parsedUser[0]), false)
 })
 
 test('Test 04', () => {
   const user = 'usr:@itziar age:19 loc:isle psw:aaa fll:222 eme:itzi@gmail.com'
   const parsedUser = parseUsers(user)
-  assert.strictEqual(isValidUser(...parsedUser), true)
+  assert.strictEqual(isValidUser(parsedUser[0]), true)
 })
