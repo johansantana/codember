a623c93fdd018a5cf4cac5e73520690e5f6a7c6d
Johan Santana <johancarlosst@gmail.com>
2022-11-22 18:05:02 -0400

    feat: added tests to challenge 04

---
+++ src/test/04.test.ts
@@ -0,0 +1,28 @@
+import { test } from 'node:test'
+import assert from 'node:assert'
+import { isValidPass } from '../04/index.js'
+
+test('Test 01', () => {
+  const password = 55678
+  assert.strictEqual(isValidPass(password), true)
+})
+
+test('Test 02', () => {
+  const password = 12555
+  assert.strictEqual(isValidPass(password), true)
+})
+
+test('Test 02', () => {
+  const password = 55555
+  assert.strictEqual(isValidPass(password), true)
+})
+
+test('Test 02', () => {
+  const password = 12345
+  assert.strictEqual(isValidPass(password), false)
+})
+
+test('Test 02', () => {
+  const password = 57775
+  assert.strictEqual(isValidPass(password), false)
+})
